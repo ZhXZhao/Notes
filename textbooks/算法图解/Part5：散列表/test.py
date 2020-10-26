ff00535008d3e53cbb2f96a9c48e39c38951e543
ZhaoZhixin <zhaozx@nuaa.edu.cn>
2020-10-26 22:10:23 +0800

    add chapter 5

---
+++ textbooks/算法图解/Part5：散列表/test.py
@@ -0,0 +1,11 @@
+'''
+Author: ZhXZhao
+Date: 2020-10-26 15:16:30
+LastEditors: ZhXZhao
+LastEditTime: 2020-10-26 15:18:29
+Description:
+'''
+mydict = dict()
+mydict["key"] = "hello"
+mydict["key"] = "world"
+print(mydict)
